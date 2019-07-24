f1e8f33e942d6f23bf0f7ae42fe7c00c644589a0
EgorovM <egorov_michil@mail.ru>
2019-07-24 21:38:43 +0900

    init

---
+++ static/assets/vendor/js/blog.js
@@ -0,0 +1,26 @@
+if($(window).width() < 992){
+    $(".navbar").addClass("bg-white");
+    console.log("asd")
+}
+
+
+
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height()-70);
+  });
+});
+
+var swiper = new Swiper('.swiper-container', {
+
+  loop: true,
+  autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});

289267303a0a17b43b21685e231ad2487a4ccc3e
EgorovM <egorov_michil@mail.ru>
2019-07-26 20:18:28 +0900

    navbar width and swiper bullets and little styles

--- main/static/assets/vendor/js/blog.js
+++ main/static/assets/vendor/js/blog.js
@@ -11,15 +11,3 @@ $(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height()-70);
   });
 });
-
-var swiper = new Swiper('.sw-cn', {
-
-  loop: true,
-  autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
-  pagination: {
-    el: '.swiper-pagination1',
-  },
-});

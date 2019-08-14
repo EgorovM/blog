bf949a70aaeb4b5be1fb3482181196492870476b
EgorovM <egorov_michil@mail.ru>
2019-08-15 00:09:47 +0900

    integrate wagtail

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'wagtail.contrib.forms',
+    'wagtail.contrib.redirects',
+    'wagtail.embeds',
+    'wagtail.sites',
+    'wagtail.users',
+    'wagtail.snippets',
+    'wagtail.documents',
+    'wagtail.images',
+    'wagtail.search',
+    'wagtail.admin',
+    'wagtail.core',
+
+    'modelcluster',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +63,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'wagtail.core.middleware.SiteMiddleware',
+    'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
+
+WAGTAIL_SITE_NAME = 'Russe Space'
 
 ROOT_URLCONF = 'blog.urls'
 
--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,19 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import path, re_path, include
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.core import urls as wagtail_urls
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include('main.urls')),
+
+    re_path(r'^cms/', include(wagtailadmin_urls)),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+    re_path(r'^pages/', include(wagtail_urls)),
 ]
 
 if settings.DEBUG:
